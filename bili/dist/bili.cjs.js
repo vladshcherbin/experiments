c505eb09d8568640db62cf80eef82a40606d4fd7
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2018-02-20 11:08:52 +0300

    Update build files

--- bili/dist/bili.cjs.js
+++ bili/dist/bili.cjs.js
@@ -2,23 +2,18 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
+const testing = true;
+class Test {
+  constructor() {
+    Object.defineProperty(this, "test", {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: 'test'
+    });
   }
+
 }
-
-var testing = true;
-var Test = function Test() {
-  _classCallCheck(this, Test);
-
-  Object.defineProperty(this, "test", {
-    configurable: true,
-    enumerable: true,
-    writable: true,
-    value: 'test'
-  });
-};
 
 exports.testing = testing;
 exports.Test = Test;
--- bili/dist/bili.es.js
+++ bili/dist/bili.es.js
@@ -1,19 +1,14 @@
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
+const testing = true;
+class Test {
+  constructor() {
+    Object.defineProperty(this, "test", {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: 'test'
+    });
   }
+
 }
-
-var testing = true;
-var Test = function Test() {
-  _classCallCheck(this, Test);
-
-  Object.defineProperty(this, "test", {
-    configurable: true,
-    enumerable: true,
-    writable: true,
-    value: 'test'
-  });
-};
 
 export { testing, Test };
